06f40267d9ddf07a87f70260eb1d80ddedfe8804
mahendarsingh1 <mahendarsinghrathod111@gmail.com>
2022-03-09 23:53:19 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import Header from "./components/header";
+import Breadcrumb from "./components/breadcrumb";
+
 function App() {
   return (
     <>
+      <Header/>
+      <Breadcrumb/>
     </>
   );
 }
